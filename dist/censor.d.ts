f19fe3173692c41e3c54ec6512e92a0354fe845b
[emresengul] <[emredeneme432@gmail.com]>
2022-06-27 00:19:27 +0300

    dist file added

---
+++ dist/censor.d.ts
@@ -0,0 +1,1 @@
+export declare const emailCensor: (email: string) => void;
---
+++ dist/censor.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.emailCensor = void 0;
+var emailCensor = function (email) {
+};
+exports.emailCensor = emailCensor;
+//# sourceMappingURL=censor.js.map
